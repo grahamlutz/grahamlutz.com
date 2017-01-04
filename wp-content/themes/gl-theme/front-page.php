d2608e36046c3d184a49a043d4c89c17ec56e3d1
Graham Lutz <graham@brainbytescreative.com>
2017-01-04 06:46:20 -0500

    moves fifth seciton php

--- wp-content/themes/gl-theme/front-page.php
+++ wp-content/themes/gl-theme/front-page.php
@@ -38,10 +38,10 @@ if( get_option( 'show_on_front' ) == 'posts' ): ?>
 else:
 
 	$sections_order_first_section = get_theme_mod( 'illdy_general_sections_order_first_section', 1 );
-	$sections_order_second_section = get_theme_mod( 'illdy_general_sections_order_second_section', 5 );
+	$sections_order_second_section = get_theme_mod( 'illdy_general_sections_order_second_section', 2 );
 	$sections_order_third_section = get_theme_mod( 'illdy_general_sections_order_third_section', 3 );
 	$sections_order_fourth_section = get_theme_mod( 'illdy_general_sections_order_fourth_section', 4 );
-	$sections_order_fifth_section = get_theme_mod( 'illdy_general_sections_order_fifth_section', 2 );
+	$sections_order_fifth_section = get_theme_mod( 'illdy_general_sections_order_fifth_section', 5 );
 	$sections_order_sixth_section = get_theme_mod( 'illdy_general_sections_order_sixth_section', 6 );
 	$sections_order_seventh_section = get_theme_mod( 'illdy_general_sections_order_seventh_section', 7 );
 	$sections_order_eighth_section = get_theme_mod( 'illdy_general_sections_order_eighth_section', 8 );
@@ -78,6 +78,10 @@ else:
 		illdy_sections_order( $sections_order_first_section );
 	endif;
 
+  if( $sections_order_fifth_section ):
+		illdy_sections_order( $sections_order_fifth_section );
+	endif;
+
 	if( $sections_order_second_section ):
 		illdy_sections_order( $sections_order_second_section );
 	endif;
@@ -88,10 +92,6 @@ else:
 
 	if( $sections_order_fourth_section ):
 		illdy_sections_order( $sections_order_fourth_section );
-	endif;
-
-	if( $sections_order_fifth_section ):
-		illdy_sections_order( $sections_order_fifth_section );
 	endif;
 
 	if( $sections_order_sixth_section ):
